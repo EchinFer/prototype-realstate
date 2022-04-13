23a4d5cac7a00546284334d133187c6cd35f72eb
Moochfer <echinfer@gmail.com>
2022-04-13 09:27:56 -0400

    build commit

---
+++ buildGithub/precache-manifest.bbba6a15200aa0a409dac951b5a40558.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a923231acd9205bd655176caba1a713",
+    "url": "/demo/react/mingrand/index.html"
+  },
+  {
+    "revision": "53a058e73419b2b32a82",
+    "url": "/demo/react/mingrand/static/js/2.f2e3018c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo/react/mingrand/static/js/2.f2e3018c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5552f2ca1fb459317a0d",
+    "url": "/demo/react/mingrand/static/js/main.dfa5a6e1.chunk.js"
+  },
+  {
+    "revision": "83227571b5237cf38b08",
+    "url": "/demo/react/mingrand/static/js/runtime-main.1e53abe0.js"
+  }
+]);
